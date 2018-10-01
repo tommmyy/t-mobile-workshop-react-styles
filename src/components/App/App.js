fb6c8a3ff0099254e22293d9823dc116d7a16c30
Tomáš Konrády <tomaskonrady@gmail.com>
2018-10-01 22:57:14 +0200

    removing forgotten tabs

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,21 +1,9 @@
 import React, { Component } from 'react';
-import Tabs from "../Tabs";
 
 class App extends Component {
 	render() {
 
-		return (
-			<Tabs>
-				<TabsMenu>
-					<Tab>Author</Tab>
-					<Tab active>Friends</Tab>
-					<Tab disabled>Friend</Tab>
-				</TabsMenu>
-				<TabsContent>
-
-				</TabsContent>
-			</Tabs>
-		);
+		return <div />;
 	}
 }
 
--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,1 @@
-export { default as Tabs } from "./Tabs";
-
 export { default as App } from "./App";
